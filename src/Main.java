964e84a1bc58fda3da8409929ed2d5cadad58b78
VovaZ <zalikvova1998@gmail.com>
2023-03-30 15:23:11 +0300

    Version 1.0

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {100,123,423423,1212312,4324234};
+        SalesManager manager = new SalesManager(sales);
+        System.out.println(manager.max());
+    }
+}
